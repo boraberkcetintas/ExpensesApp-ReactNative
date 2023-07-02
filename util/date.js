6dd0f222c89fab23a5f36abf8201d01dcde52735
Bora Berk <87586166+boraberkcetintas@users.noreply.github.com>
2023-07-02 17:00:51 +0300

    formatted date util

---
+++ util/date.js
@@ -0,0 +1,3 @@
+export function getFormattedDate(date) {
+  return( `${date.getDate() < 10 ? "0" : ""}${date.getDate()}.${date.getMonth() < 10 ? "0" : ""}${date.getMonth() + 1}.${date.getFullYear()}`);
+}
