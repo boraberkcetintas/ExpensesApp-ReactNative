7ed9d9f969779f83616d760a6d8a2dae1083cba6
Bora Berk <87586166+boraberkcetintas@users.noreply.github.com>
2023-07-03 23:11:08 +0300

    Expenses output added
    Expenses output added to the recent and the all expenses screens

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -3,7 +3,13 @@ import { useSelector } from "react-redux";
 
 function AllExpenses() {
   const expenses = useSelector((state) => state.expensesReducer.expenses);
-  return <ExpensesOutput expensePeriodName={"Total"} expenses={expenses} />;
+  return (
+    <ExpensesOutput
+      expensePeriodName={"Total"}
+      expenses={expenses}
+      fallbackText="No registered expenses found!"
+    />
+  );
 }
 
 export default AllExpenses;
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,14 +3,20 @@ import { useSelector } from "react-redux";
 import { getDateMinusDays } from "../util/date";
 function RecentExpenses() {
   const expenses = useSelector((state) => state.expensesReducer.expenses);
+
   const recentExpenses = expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-
-    return (expense.date >= date7DaysAgo) && (expense.date <= today);
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
-  return <ExpensesOutput expensePeriodName={"Last 7 Days"} expenses={recentExpenses}/>;
+  return (
+    <ExpensesOutput
+      expensePeriodName={"Last 7 Days"}
+      expenses={recentExpenses}
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
 }
 
 export default RecentExpenses;
